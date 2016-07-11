d784c58882b7f5c78bcf16ee2d727db9147568f1
Nirbheek Chauhan <nirbheek@centricular.com>
2016-07-11 18:55:50 +0530

    meson: Detect perl at configure time
    This allows the build file to be used with a faulty environment. Very
    useful on Windows where the configuration might happen inside a Cerbero
    shell and the building inside Visual Studio.

--- gio/build_mkenum.py
+++ gio/build_mkenum.py
@@ -7,15 +7,16 @@
 
 import sys, os, shutil, subprocess
 
-glib_mkenums = sys.argv[1]
-ofilename = sys.argv[2]
-template_file_dir = sys.argv[3]
+perl = sys.argv[1]
+glib_mkenums = sys.argv[2]
+ofilename = sys.argv[3]
+template_file_dir = sys.argv[4]
 template_file_path = template_file_dir + '/' + ofilename + '.template'
-headers = sys.argv[4:]
+headers = sys.argv[5:]
 
 arg_array = ['--template', template_file_path]
 
-cmd = [shutil.which('perl'), glib_mkenums]
+cmd = [perl, glib_mkenums]
 pc = subprocess.Popen(cmd + arg_array + headers, stdout=subprocess.PIPE)
 (stdo, _) = pc.communicate()
 if pc.returncode != 0:
